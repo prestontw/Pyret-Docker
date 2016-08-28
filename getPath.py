7fda569ca54446be5c67708ee7c37aad0889edc1
Preston Tunnell Wilson <tunpm-16@rhodes.edu>
2016-08-28 19:24:32 -0400

    add code that will compile from arr to js

--- getPath.py
+++ getPath.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+
 import argparse
 from pathlib import Path
+from subprocess import call
 
 parser = argparse.ArgumentParser(description='Compile a pyret program.')
 parser.add_argument('file', type=str, help='input pyret file')
@@ -11,4 +14,14 @@ print("input file:", args.file)
 print("output file:", args.output)
 
 infile = Path(args.file)
-print(infile.resolve())
+infileAbsPath = infile.resolve()
+print(infileAbsPath)
+print(infileAbsPath.parent)
+
+call(["node", "build/phas0/pyret.jarr",\
+      "--build-runnable", infileAbsPath,\
+      "--outfile", infileAbsPath.parent / args.output,\
+      "--builtin-js-dir", "src/js/trove/",\
+      "--builtin-arr-dir", "src/arr/trove",\
+      "--require-config", "src/scripts/standalone-configA.json"\
+      ])
