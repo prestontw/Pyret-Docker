b18ddfb9846a0f4cba3c921121f018bdb3d4aa89
Preston Tunnell Wilson <tunpm-16@rhodes.edu>
2016-08-28 19:33:52 -0400

    add debug option
    and refactor input and output file for node code

--- getPath.py
+++ getPath.py
@@ -7,20 +7,28 @@ from subprocess import call
 parser = argparse.ArgumentParser(description='Compile a pyret program.')
 parser.add_argument('file', type=str, help='input pyret file')
 parser.add_argument('-o', '--output', dest='output', default='pyret.out', help='pyret output executable name')
+parser.add_argument('--debug', action="store_true", help='debug compiling script')
 
 args = parser.parse_args()
 
-print("input file:", args.file)
-print("output file:", args.output)
+if args.debug:
+    print("input file:", args.file)
+    print("output file:", args.output)
 
 infile = Path(args.file)
 infileAbsPath = infile.resolve()
-print(infileAbsPath)
-print(infileAbsPath.parent)
+fileCommonDirectory = infileAbsPath.parent
 
-call(["node", "build/phas0/pyret.jarr",\
-      "--build-runnable", infileAbsPath,\
-      "--outfile", infileAbsPath.parent / args.output,\
+infileStr = str(fileCommonDirectory / args.file)
+outfileStr = str(fileCommonDirectory / args.output)
+
+if args.debug:
+    print("abs input file:", infileStr)
+    print("abs output file:", outfileStr)
+
+call(["node", "build/phase0/pyret.jarr",\
+      "--build-runnable", str(fileCommonDirectory / args.file),\
+      "--outfile", str(fileCommonDirectory / args.output),\
       "--builtin-js-dir", "src/js/trove/",\
       "--builtin-arr-dir", "src/arr/trove",\
       "--require-config", "src/scripts/standalone-configA.json"\
